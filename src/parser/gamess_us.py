7d146f0e8a69ac8bb5357bbb4ec928e7fd77e691
Thomas Applencourt <t.applencourt@gmail.com>
2015-03-17 17:32:37 +0100

    Adding parser...

---
+++ src/parser/gamess_us.py
@@ -0,0 +1,138 @@
+#  __
+# /__  _. ._ _   _   _  _        _
+# \_| (_| | | | (/_ _> _>   |_| _>
+#
+
+from src.parser_handler import get_dict_ele
+import re
+
+
+def parse_basis_data_gamess_us(data, name, des, elts, debug=False):
+    """Parse the basis data raw html of gamess-us to get a nice tuple
+       Return (name, description, [[ele, data_ele],...])"""
+    basis_data = []
+
+    b = data.find("$DATA")
+    e = data.find("$END")
+    if (b == -1 or data.find("$DATA$END") != -1):
+        if debug:
+            print data
+        raise Exception("WARNING not DATA")
+    else:
+        dict_replace = {"PHOSPHOROUS": "PHOSPHORUS",
+                        "D+": "E+",
+                        "D-": "E-"}
+
+        for k, v in dict_replace.iteritems():
+            data = data.replace(k, v)
+
+        data = data[b + 5:e - 1].split('\n\n')
+
+        dict_ele = get_dict_ele()
+
+        for (elt, data_elt) in zip(elts, data):
+
+            elt_long_th = dict_ele[elt.lower()]
+            elt_long_exp = data_elt.split()[0].lower()
+
+            if "$" in data_elt:
+                if debug:
+                    print "Eror",
+                raise Exception("WARNING bad split")
+
+            if elt_long_th == elt_long_exp:
+                basis_data.append([elt, data_elt.strip()])
+            else:
+                if debug:
+                    print "th", elt_long_th
+                    print "exp", elt_long_exp
+                    print "abv", elt
+                raise Exception("WARNING not a good ELEMENT")
+
+    return (name, des, basis_data)
+
+
+symmetry_regex = re.compile(ur'^(\w)\s+\d+\b')
+
+
+def l_symmetry_gamess_us(atom_basis):
+    """
+    Return the begin and the end of all the type of orbital
+    input: atom_basis = [name, S 1, 12 0.12 12212, ...]
+    output: [ [type, begin, end], ...]
+    """
+    # Example
+    # [[u'S', 1, 5], [u'L', 5, 9], [u'L', 9, 12], [u'D', 16, 18]]"
+
+    l = []
+    for i, line in enumerate(atom_basis):
+        # Optimisation for not seaching all the time
+        if len(line) < 10:
+            m = re.search(symmetry_regex, line)
+            if m:
+                # Cause of L !
+                read_symmetry = m.group(1)
+
+                # L is real L or special SP
+                # Just check the number of exponant
+                if all([read_symmetry == "L",
+                        len(atom_basis[i + 1].split()) == 4]):
+                    real_symmetry = "SP"
+                else:
+                    real_symmetry = read_symmetry
+
+                l.append([real_symmetry, i])
+                try:
+                    l[-2].append(i)
+                except IndexError:
+                    pass
+
+    l[-1].append(i + 1)
+    return l
+
+
+def handle_l_gamess_us(l_atom_basis):
+    """
+    Read l_atom_basis and change the SP in L and P
+    """
+
+    l_data = []
+    for atom_basis in l_atom_basis:
+
+        # Split the data in line
+        l_line_raw = atom_basis.split("\n")
+        l_line = [l_line_raw[0]]
+        # l_line_raw[0] containt the name of the Atom
+
+        for symmetry, begin, end in l_symmetry_gamess_us(l_line_raw):
+
+            if symmetry == "SP":
+
+                body_s = []
+                body_p = []
+
+                for i_l in l_line_raw[begin + 1:end]:
+
+                    # one L =>  S & P
+                    a = i_l.split()
+
+                    common = "{:>3}".format(a[0])
+                    common += "{:>15.7f}".format(float(a[1]))
+
+                    tail_s = common + "{:>23.7f}".format(float(a[2]))
+                    body_s.append(tail_s)
+
+                    tail_p = common + "{:>23.7f}".format(float(a[3]))
+                    body_p.append(tail_p)
+
+                l_line += [l_line_raw[begin].replace("L", "S")]
+                l_line += body_s
+
+                l_line += [l_line_raw[begin].replace("L", "P")]
+                l_line += body_p
+            else:
+                l_line += l_line_raw[begin:end]
+
+        l_data.append("\n".join(l_line))
+
+    return l_data
---
+++ src/parser/nwchem.py
@@ -0,0 +1,228 @@
+#            _
+# |\ |      /  |_   _  ._ _
+# | \| \/\/ \_ | | (/_ | | |
+#
+import json
+
+
+def extract_basis_nwchem(data, name):
+    """Extract atomic orbital, charge density fitting, or exchange
+    correlation functional basis data from a text region passed in as
+    data. The charge density fitting and exchange correlation functional
+    basis set data are employed for density functional calculations.
+
+    @param data: text region containing basis set data
+    @type data : str
+    @param name: name of basis type: "ao basis", "cd basis", or "xc basis"
+    @type name : str
+    @return: per-element basis set chunks
+    @rtype : list
+    """
+
+    begin_marker = """BASIS "{0}" PRINT""".format(name)
+    end_marker = "END"
+
+    # search for the basis set data begin marker
+    # calling "upper" on data because original data has inconsistent
+    # capitalization
+    begin = data.upper().find(begin_marker.upper())
+    end = data.upper().find(end_marker, begin)
+
+    # No basis data found
+    if begin == -1:
+        return []
+
+    trimmed = data[begin + len(begin_marker): end - len(end_marker)].strip()
+
+    chunks = []
+    lines = []
+
+    # group lines of data delimited by #BASIS SET... into per-element chunks
+    for line in trimmed.split("\n"):
+        if line.upper().startswith("#BASIS SET"):
+            if lines:
+                chunks.append(lines)
+            lines = [line]
+        else:
+            lines.append(line)
+
+    # handle trailing chunk that is not followed by another #BASIS SET...
+    if lines and (not chunks or lines != chunks[-1]):
+        chunks.append(lines)
+
+    # join lines back into solid text blocks
+    chunks = ["\n".join(c) for c in chunks]
+    return chunks
+
+
+def extract_ecp_nwchem(data):
+    """Extract the effective core potential basis data from a text region
+    passed in as data.
+
+    @param data: text region containing ECP data
+    @type data : str
+    @return: per-element effective core potential chunks
+    @rtype : list
+    """
+
+    ecp_begin_mark = "ECP\n"
+    ecp_end_mark = "END"
+    ecp_begin = data.upper().find(ecp_begin_mark)
+    ecp_end = data.upper().find(ecp_end_mark, ecp_begin)
+    ecp_region = ""
+
+    if ecp_begin > -1 and ecp_end > -1:
+        ecp_region = data[
+            ecp_begin +
+            len(ecp_begin_mark): ecp_end -
+            len(ecp_end_mark)].strip()
+
+    # No ECP data, so return empty list
+    else:
+        return []
+
+    chunks = []
+    lines = []
+
+    # group lines of data delimited by XX nelec YY into chunks, e.g.
+    # "Zn nelec 18" begins a zinc ECP
+    for line in ecp_region.split("\n"):
+        if line.lower().find(" nelec ") > -1:
+            if lines:
+                chunks.append(lines)
+            lines = [line]
+        else:
+            lines.append(line)
+
+    # handle trailing chunk that is not followed by another XX nelec YY..
+    if lines and (not chunks or lines != chunks[-1]):
+        chunks.append(lines)
+
+    # join lines back into solid text blocks
+    chunks = ["\n".join(c) for c in chunks]
+    return chunks
+
+
+def unpack_nwchem_basis_block(data):
+    """Unserialize a NWChem basis data block and extract components
+
+    @param data: a JSON of basis set data, perhaps containing many types
+    @type data : str
+    @return: unpacked data
+    @rtype : dict
+    """
+
+    unpacked = json.loads(data)
+    return unpacked
+
+
+def parse_basis_data_nwchem(data, name, description, elements, debug=True):
+    """Parse the NWChem basis data raw html to get a nice tuple.
+
+    The data-pairs item is actually expected to be a 2 item list:
+    [symbol, data]
+
+    e.g. ["Ca", "#BASIS SET..."]
+
+    @param data: raw HTML from BSE
+    @type data : unicode
+    @param name: basis set name
+    @type name : str
+    @param des: basis set description
+    @type des : str
+    @param elements: element symbols e.g. ['H', 'C', 'N', 'O', 'Cl']
+    @type elements : list
+    @return: (name, description, data-pairs)
+    @rtype : tuple
+    """
+
+    unused_elements = set([e.upper() for e in elements])
+
+    def extract_symbol(txt):
+        for sline in txt.split("\n"):
+            if not sline.startswith("#"):
+                try:
+                    symbol = sline[:3].strip().split()[0]
+                    return symbol
+                except IndexError:
+                    continue
+
+        raise ValueError("Can't find element symbol in {0}".format(txt))
+
+    ao_chunks = extract_basis_nwchem(data, "ao basis")
+    cd_chunks = extract_basis_nwchem(data, "cd basis")
+    xc_chunks = extract_basis_nwchem(data, "xc basis")
+    ecp_chunks = extract_ecp_nwchem(data)
+
+    if not any([ao_chunks, cd_chunks, xc_chunks, ecp_chunks]):
+        str_ = "No basis set data found while attempting to process {0} ({1})"
+        raise ValueError(str_.format(name, description))
+
+    # Tag all used elements, whether from ordinary AO basis or ECP section
+    for chunk in ao_chunks + cd_chunks + xc_chunks + ecp_chunks:
+        try:
+            symbol = extract_symbol(chunk)
+            unused_elements.remove(symbol.upper())
+        except KeyError:
+            pass
+
+    if unused_elements:
+        msg = "Warning: elements {0} left over for {1}"
+        print msg.format(list(unused_elements), name)
+
+    # Form packed chunks, turn packed chunks into pairs
+    used_elements = set()
+    packed = {}
+
+    for cgroup, gname in [(ao_chunks, "ao basis"), (cd_chunks, "cd basis"),
+                          (xc_chunks, "xc basis"), (ecp_chunks, "ecp")]:
+        for chunk in cgroup:
+            symbol = extract_symbol(chunk)
+
+            # Expand entry, e.g. add ecp data for Na after it has ao basis
+            try:
+                idx, ch = packed[symbol]
+                ch[gname] = chunk
+                chunk_dict = ch.copy()
+            # Create fresh entry, e.g. add Na with initial ao basis
+            except KeyError:
+                chunk_dict = {gname: chunk}
+                idx = len(used_elements)
+                used_elements.add(symbol)
+
+            packed[symbol] = (idx, chunk_dict)
+
+    """
+        for chunk in ao_chunks:
+            symbol = extract_symbol(chunk)
+            chunk_dict = {"ao basis" : chunk}
+            idx = len(used_elements)
+            used_elements.add(symbol)
+            packed[symbol] = (idx, chunk_dict)
+
+        for chunk in ecp_chunks:
+            symbol = extract_symbol(chunk)
+            #add ECP data if existing chunk, else create fresh chunk
+            try:
+                idx, ch = packed[symbol]
+                ch["ecp"] = chunk
+                chunk_dict = ch.copy()
+            except KeyError:
+                chunk_dict = {"ecp" : chunk}
+                idx = len(used_elements)
+                used_elements.add(symbol)
+            packed[symbol] = (idx, chunk_dict)
+        """
+
+    values = sorted(packed.values())
+
+    # Assign (Symbol, Serialized) to final pairs
+    pairs = []
+    for idx, chunk in values:
+        symbol = extract_symbol(chunk.get("ao basis")
+                                or chunk.get("cd basis")
+                                or chunk.get("xc basis")
+                                or chunk.get("ecp"))
+        serialized = json.dumps(chunk)
+        pairs.append([symbol, serialized])
+    return [name, description, pairs]
